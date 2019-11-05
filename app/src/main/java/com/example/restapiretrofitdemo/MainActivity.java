dcfc70df0ba47576b344e281faeef29a2e54e70e
AshrafulMarufAnik <ashraful.maruf.anik@gmail.com>
2019-11-06 00:42:31 +0600

    Data parsing with volley

--- app/src/main/java/com/example/restapiretrofitdemo/MainActivity.java
+++ app/src/main/java/com/example/restapiretrofitdemo/MainActivity.java
@@ -86,7 +86,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onRefresh() {
                 jsonParseUsingVolley();
-
             }
         });
 
