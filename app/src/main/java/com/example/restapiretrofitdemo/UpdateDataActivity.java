c172ccf83a6768a8d76c43378d4aaf8d91c46fd1
AshrafulMarufAnik <ashraful.maruf.anik@gmail.com>
2019-11-06 17:32:00 +0600

    update operation

--- app/src/main/java/com/example/restapiretrofitdemo/UpdateDataActivity.java
+++ app/src/main/java/com/example/restapiretrofitdemo/UpdateDataActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
@@ -19,10 +20,13 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UpdateDataActivity extends AppCompatActivity {
-    private EditText updateET1,updateET2,updateET3,updateET4;
+    private EditText updateET1, updateET2, updateET3, updateET4;
     private Button updateBTN;
-    private String userId,name,title,body,status;
+    private String userId = "", name = "", title = "", body = "", status = "";
     private RequestQueue requestQueue;
 
     @Override
@@ -33,7 +37,7 @@ public class UpdateDataActivity extends AppCompatActivity {
         init();
         requestQueue = Volley.newRequestQueue(this);
 
-        if(getIntent().getExtras()!=null){
+        if (getIntent().getExtras() != null) {
             userId = getIntent().getStringExtra("userId");
             name = getIntent().getStringExtra("name");
             title = getIntent().getStringExtra("title");
@@ -49,45 +53,51 @@ public class UpdateDataActivity extends AppCompatActivity {
         updateBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String userId = updateET1.getText().toString();
-                String name = updateET1.getText().toString();
-                String title = updateET1.getText().toString();
-                String body = updateET1.getText().toString();
 
-                if(userId != null && name != null && title!= null && body != null){
-                    try {
-                        updateDataToApi(userId,name,title,body,status);
+                if(updateET1.getText().toString()!=null && updateET2.getText().toString()!=null
+                    && updateET3.getText().toString()!=null && updateET4.getText().toString()!=null){
 
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
+                    String userId = updateET1.getText().toString();
+                    String name = updateET1.getText().toString();
+                    String title = updateET1.getText().toString();
+                    String body = updateET1.getText().toString();
+
+                    updateDataToApi(userId, name, title, body, status);
+
                 }
                 else {
-                    Toast.makeText(UpdateDataActivity.this, "No data to update", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(UpdateDataActivity.this, "Enter data", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
     }
 
-    private void updateDataToApi(String userId, String name, String title, String body, String status) throws JSONException {
+    private void updateDataToApi(String userId, String name, String title, String body, String status) {
+
         String POST_URL = "http://192.168.11.216:80/api/user/update.php";
 
         JSONObject jsonBody = new JSONObject();
-        jsonBody.put("userId", userId);
-        jsonBody.put("name", name);
-        jsonBody.put("title", title);
-        jsonBody.put("body", body);
-        jsonBody.put("status", status);
+        try {
+            jsonBody.put("userId", userId);
+            jsonBody.put("name", name);
+            jsonBody.put("title", title);
+            jsonBody.put("body", body);
+            jsonBody.put("status", status);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Log.i("jsonBody", String.valueOf(jsonBody));
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, POST_URL, jsonBody,
                 new com.android.volley.Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        Toast.makeText(UpdateDataActivity.this, "Update POST: "+response.toString(), Toast.LENGTH_SHORT).show();
-                        Log.d("Update POST status: ",response.toString());
+                        Toast.makeText(UpdateDataActivity.this, "Update POST: " + response.toString(), Toast.LENGTH_SHORT).show();
+                        Log.d("Update POST status: ", response.toString());
 
-                        Intent intent = new Intent(UpdateDataActivity.this,MainActivity.class);
+                        Intent intent = new Intent(UpdateDataActivity.this, MainActivity.class);
                         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                         startActivity(intent);
                         finish();
@@ -96,7 +106,7 @@ public class UpdateDataActivity extends AppCompatActivity {
                 }, new com.android.volley.Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(UpdateDataActivity.this, "POST Error: "+error.getMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(UpdateDataActivity.this, "POST Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
                 error.printStackTrace();
             }
         });
